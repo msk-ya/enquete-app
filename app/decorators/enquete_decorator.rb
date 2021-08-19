b0d4a93b6632ba75afa9b202716415f771071fee
msk-ya <masakiabineko@gmail.com>
2021-08-19 18:57:45 +0900

    アンケート終了分岐追加

--- app/decorators/enquete_decorator.rb
+++ app/decorators/enquete_decorator.rb
@@ -10,5 +10,19 @@ module EnqueteDecorator
     end
   end
   
+  #期限の状況で回答ボタンを表示するか分岐
+  def answer_button_existence
+    today = Date.today
+    if self.limit >today
+      return "<a herf='enquetes/#{self.id}/results' class='enquete-answer-button'>
+               <img src='/svg/write_white.svg' width='20' height='20' style='color:white' />アンケートに答える
+              </a>".html_safe
+    else
+     return "募集を終了しました。"
+    end
+    
+  end
+  
+  
   
 end
